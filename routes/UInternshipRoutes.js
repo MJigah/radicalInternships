6ae6e95fd48414038cbd1a321be0669ce833b8cd
MJigah <jigah4july@gmailcom>
2022-11-02 00:46:15 +0100

    updating user intern routes

--- routes/UInternshipRoutes.js
+++ routes/UInternshipRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
-const {addUserIntern} = require('../controllers/userInternship');
+const {addUserIntern} = require('../controllers/userInternshipController');
 const router = express.Router();
 
 /**
